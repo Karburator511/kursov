9e3e34260a77fa9588262621cfff4355ba0e33f7
Игорь Лeвкун <igorlevkun@outlook.com>
2024-12-15 00:51:59 +0300

    добавлено меню

--- kursov/kursov.cpp
+++ kursov/kursov.cpp
@@ -1,151 +1,230 @@
 ﻿#include <iostream>
-#include <vector>
 #include <fstream>
-#include <iomanip>
-#include <string>
+#include <vector>
+#include <cstring>
 
 using namespace std;
 
-// Тип для представления матрицы
-typedef vector<vector<int>> Matrix;
+class Matrix {
+public:
+    int size;  // Размерность матрицы
+    int** data;  // Данные матрицы
 
-// Переменные для локализации
-string msgEnterMatrixSize = "Введите размер матриц: ";
-string msgInputMatrix = "Ввод матрицы ";
-string msgMatrixSaved = "Матрица сохранена в файл ";
-string msgMatrixLoaded = "Матрица загружена из файла ";
-string msgFileError = "Ошибка открытия файла ";
-string msgEditElement = "Введите номер строки и столбца для редактирования (начиная с 0): ";
-string msgEnterNewValue = "Введите новое значение: ";
-string msgInvalidIndices = "Некорректные индексы.";
-string msgMatrixResult = "Матрица C (результат):";
-
-// Функция для создания квадратной матрицы заданного размера
-Matrix createMatrix(int size) {
-    return Matrix(size, vector<int>(size, 0));
-}
-
-// Функция для ввода матрицы с консоли
-void inputMatrix(Matrix& matrix, const string& matrixName) {
-    int size = matrix.size();
-    cout << msgInputMatrix << matrixName << ":" << endl;
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            cin >> matrix[i][j];
+    // Конструктор для создания матрицы заданного размера
+    Matrix(int n) : size(n) {
+        data = new int* [size];
+        for (int i = 0; i < size; ++i) {
+            data[i] = new int[size];
         }
     }
-}
 
-// Функция для вывода матрицы на консоль
-void printMatrix(const Matrix& matrix) {
-    for (const auto& row : matrix) {
-        for (const auto& elem : row) {
-            cout << setw(5) << elem;
+    // Деструктор для освобождения памяти
+    ~Matrix() {
+        for (int i = 0; i < size; ++i) {
+            delete[] data[i];
         }
-        cout << endl;
+        delete[] data;
     }
-}
 
-// Функция для записи матрицы в файл
-void saveMatrixToFile(const Matrix& matrix, const string& filename) {
-    ofstream file(filename);
-    if (file.is_open()) {
-        for (const auto& row : matrix) {
-            for (const auto& elem : row) {
-                file << elem << " ";
-            }
-            file << endl;
-        }
-        file.close();
-        cout << msgMatrixSaved << filename << endl;
-    }
-    else {
-        cerr << msgFileError << filename << endl;
-    }
-}
-
-// Функция для чтения матрицы из файла
-void loadMatrixFromFile(Matrix& matrix, const string& filename) {
-    ifstream file(filename);
-    if (file.is_open()) {
-        for (auto& row : matrix) {
-            for (auto& elem : row) {
-                file >> elem;
+    // Функция для ввода данных из консоли
+    void input() {
+        cout << "Введите элементы матрицы (" << size << "x" << size << "):\n";
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                cin >> data[i][j];
             }
         }
-        file.close();
-        cout << msgMatrixLoaded << filename << endl;
     }
-    else {
-        cerr << msgFileError << filename << endl;
-    }
-}
 
-// Функция для редактирования элемента матрицы
-void editMatrixElement(Matrix& matrix) {
-    int i, j, value;
-    cout << msgEditElement;
-    cin >> i >> j;
-    if (i >= 0 && i < matrix.size() && j >= 0 && j < matrix.size()) {
-        cout << msgEnterNewValue;
+    // Функция для вывода матрицы в консоль
+    void output() const {
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                cout << data[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    // Функция для записи матрицы в файл
+    void saveToFile(const string& filename) const {
+        ofstream out(filename);
+        if (!out) {
+            cout << "Ошибка при открытии файла для записи.\n";
+            return;
+        }
+        out << size << endl;
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                out << data[i][j] << " ";
+            }
+            out << endl;
+        }
+        out.close();
+    }
+
+    // Функция для чтения матрицы из файла
+    void loadFromFile(const string& filename) {
+        ifstream in(filename);
+        if (!in) {
+            cout << "Ошибка при открытии файла для чтения.\n";
+            return;
+        }
+        in >> size;
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                in >> data[i][j];
+            }
+        }
+        in.close();
+    }
+
+    // Функция для редактирования элемента матрицы
+    void editElement() {
+        int row, col, value;
+        cout << "Введите номер строки и столбца для редактирования (1-" << size << "): ";
+        cin >> row >> col;
+        cout << "Введите новое значение: ";
         cin >> value;
-        matrix[i][j] = value;
+        data[row - 1][col - 1] = value;
     }
-    else {
-        cout << msgInvalidIndices << endl;
-    }
-}
 
-// Функция для записи в матрицу C строк матрицы A, совпадающих с любым столбцом матрицы B
-void fillMatrixC(const Matrix& A, const Matrix& B, Matrix& C) {
-    int size = A.size();
-    for (int i = 0; i < size; ++i) {
-        bool matchFound = false;
-        for (int j = 0; j < size; ++j) {
-            bool isMatch = true;
-            for (int k = 0; k < size; ++k) {
-                if (A[i][k] != B[k][j]) {
-                    isMatch = false;
+    // Функция для проверки совпадения строки с столбцом
+    bool compareRowWithColumn(int row, int col) const {
+        for (int i = 0; i < size; ++i) {
+            if (data[row][i] != data[i][col]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Функция для записи строк из A в C, если они совпадают с каким-либо столбцом B
+    void processMatrix(const Matrix& B, Matrix& C) {
+        for (int i = 0; i < size; ++i) {
+            bool matchFound = false;
+            for (int j = 0; j < size; ++j) {
+                if (compareRowWithColumn(i, j)) {
+                    matchFound = true;
                     break;
                 }
             }
-            if (isMatch) {
-                matchFound = true;
-                break;
+            for (int j = 0; j < size; ++j) {
+                C.data[i][j] = matchFound ? data[i][j] : 0;
             }
         }
-        if (matchFound) {
-            C[i] = A[i];
-        }
-        else {
-            C[i] = vector<int>(size, 0);
-        }
     }
+};
+
+// Функция для отображения главного меню
+void showMenu() {
+    cout << "\nГлавное меню:\n";
+    cout << "1. Ввод данных для матрицы\n";
+    cout << "2. Вывод данных матриц\n";
+    cout << "3. Сохранить матрицу в файл\n";
+    cout << "4. Загрузить матрицу из файла\n";
+    cout << "5. Редактировать элемент матрицы\n";
+    cout << "6. Обработать матрицы A и B, записать результат в C\n";
+    cout << "0. Выход\n";
 }
 
+// Главная функция
 int main() {
     setlocale(LC_ALL, "Russian");
+
     int size;
-    cout << msgEnterMatrixSize;
+    cout << "Введите размерность матрицы: ";
     cin >> size;
 
-    Matrix A = createMatrix(size);
-    Matrix B = createMatrix(size);
-    Matrix C = createMatrix(size);
+    // Создаем три матрицы A, B и C
+    Matrix A(size), B(size), C(size);
 
-    inputMatrix(A, "A");
-    inputMatrix(B, "B");
+    int choice;
+    while (true) {
+        showMenu();
+        cout << "Ваш выбор: ";
+        cin >> choice;
 
-    fillMatrixC(A, B, C);
+        switch (choice) {
+        case 1:
+            cout << "Выберите матрицу для ввода данных:\n";
+            cout << "1. Матрица A\n";
+            cout << "2. Матрица B\n";
+            cout << "3. Матрица C\n";
+            int matChoice;
+            cin >> matChoice;
+            if (matChoice == 1) A.input();
+            else if (matChoice == 2) B.input();
+            else if (matChoice == 3) C.input();
+            break;
 
-    cout << "Матрица A:" << endl;
-    printMatrix(A);
+        case 2:
+            cout << "Матрица A:\n";
+            A.output();
+            cout << "Матрица B:\n";
+            B.output();
+            cout << "Матрица C:\n";
+            C.output();
+            break;
 
-    cout << "Матрица B:" << endl;
-    printMatrix(B);
+        case 3:
+            cout << "Выберите матрицу для сохранения:\n";
+            cout << "1. Матрица A\n";
+            cout << "2. Матрица B\n";
+            cout << "3. Матрица C\n";
+            cin >> matChoice;
+            {
+                string filename;
+                cout << "Введите имя файла: ";
+                cin >> filename;
+                if (matChoice == 1) A.saveToFile(filename);
+                else if (matChoice == 2) B.saveToFile(filename);
+                else if (matChoice == 3) C.saveToFile(filename);
+            }
+            break;
 
-    cout << msgMatrixResult << endl;
-    printMatrix(C);
+        case 4:
+            cout << "Выберите матрицу для загрузки:\n";
+            cout << "1. Матрица A\n";
+            cout << "2. Матрица B\n";
+            cout << "3. Матрица C\n";
+            cin >> matChoice;
+            {
+                string filename;
+                cout << "Введите имя файла: ";
+                cin >> filename;
+                if (matChoice == 1) A.loadFromFile(filename);
+                else if (matChoice == 2) B.loadFromFile(filename);
+                else if (matChoice == 3) C.loadFromFile(filename);
+            }
+            break;
+
+        case 5:
+            cout << "Выберите матрицу для редактирования:\n";
+            cout << "1. Матрица A\n";
+            cout << "2. Матрица B\n";
+            cout << "3. Матрица C\n";
+            cin >> matChoice;
+            if (matChoice == 1) A.editElement();
+            else if (matChoice == 2) B.editElement();
+            else if (matChoice == 3) C.editElement();
+            break;
+
+        case 6:
+            A.processMatrix(B, C);
+            cout << "Матрица C после обработки:\n";
+            C.output();
+            break;
+
+        case 0:
+            cout << "Выход из программы.\n";
+            return 0;
+
+        default:
+            cout << "Неверный выбор. Попробуйте снова.\n";
+            break;
+        }
+    }
 
     return 0;
 }
